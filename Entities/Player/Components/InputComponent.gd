5eea52491554c1f5c209bdcd81547de91f304fa8
Ryan Maybury <ryanmaybury1234@gmail.com>
2026-07-04 12:08:36 +0100

    Split input component into gameplay and menu actions.
    TODO: Create control action sets, one gameplay and one menu. Then, check it and determine which functions run. So if it's gameplay, then the _handle_gameplay_actions func will be callled, where as menu would do the opposite.

--- Entities/Player/Components/InputComponent.gd
+++ Entities/Player/Components/InputComponent.gd
@@ -17,16 +17,13 @@ var is_jumping: bool
 var _last_move_dir := Vector2.ZERO
 
 func _unhandled_input(event: InputEvent) -> void:
+	_handle_menu_actions(event)
+	_handle_gameplay_actions(event)
+
+func _handle_gameplay_actions(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		on_camera_input_change.emit(event.relative)
-		
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_ESCAPE:
-			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-			else:
-				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-				
+	
 	if event.is_action_pressed("jump"):
 		jump_pressed.emit()
 		is_jumping = true
@@ -53,8 +50,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 	if event.is_action("prone"):
 		is_prone = event.is_pressed()
+		
+func _handle_menu_actions(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			else:
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _process(_delta: float) -> void:
+	_handle_continuous_gameplay_actions(_delta)
+	
+func _handle_continuous_gameplay_actions(_delta: float) -> void:
 	var move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	if move_dir != _last_move_dir:
 		_last_move_dir = move_dir
